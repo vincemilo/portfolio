e516714aee806c892b40b96706c67ac38822ed6f
Vince Milo <1554999+vincemilo@users.noreply.github.com>
2024-03-27 13:31:33 -0600

    refactored projects as a class and added weather api app

---
+++ project.js
@@ -0,0 +1,9 @@
+export default class Project {
+  constructor(name, img, descripton, code, live) {
+    (this.name = name),
+      (this.img = img),
+      (this.descripton = descripton),
+      (this.code = code),
+      (this.live = live);
+  }
+}
